e15eefdc38950bcc600dc7dc0a6716908cfa4623
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-05-31 23:22:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,8 @@ if url:
         filename = f"cropped_{label.item()}.jpg"
         cropped_filenames.append(filename)
 
-        # Save the cropped image
-        cropped_image.save(filename, format='JPEG')
+        # Save the cropped image in JPEG format
+        cropped_image.convert('RGB').save(filename, format='JPEG')
 
     # Display the input image
     st.image(image, caption="Input Image", use_column_width=True)
@@ -94,5 +94,5 @@ if url:
                 col.image(cropped_image, caption=f"Cropped Image", use_column_width=True)
 
                 # Add a download button for the cropped image
-               download_button_str = f"Download {filename}"
+                download_button_str = f"Download {filename}"
                 col.markdown(f'<a href="data:application/octet-stream;base64,{base64.b64encode(open(filename, "rb").read()).decode()}"><button type="button">{download_button_str}</button></a>', unsafe_allow_html=True)
