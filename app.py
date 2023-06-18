d97717674f99c0a159bf3e17a8a1181e3a56b9df
aman-8055 <134951454+aman-8055@users.noreply.github.com>
2023-06-18 13:00:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,32 +11,66 @@ image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
 # Streamlit app
 st.title("Object Detection with YOLO")
 
-# Display an input file uploader for the image
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+# Display a radio button to choose the input type
+input_type = st.radio("Select input type", ("Image URL", "File Upload"))
 
-if uploaded_file is not None:
-    # Load the uploaded image
-    image = Image.open(uploaded_file)
+if input_type == "Image URL":
+    # Display an input text box for the image URL
+    url = st.text_input("Enter the image URL:")
+    if url:
+        # Load the image from the provided URL
+        image = Image.open(requests.get(url, stream=True).raw)
 
-    # Process the image using the YOLO model
-    inputs = image_processor(images=image, return_tensors="pt")
-    outputs = model(**inputs)
+        # Process the image using the YOLO model
+        inputs = image_processor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
 
-    # Predict bounding boxes and classes
-    logits = outputs.logits
-    bboxes = outputs.pred_boxes
+        # Predict bounding boxes and classes
+        logits = outputs.logits
+        bboxes = outputs.pred_boxes
 
-    # Post-process the object detection results
-    target_sizes = torch.tensor([image.size[::-1]])
-    results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
+        # Post-process the object detection results
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
 
-    # Display the detected objects and their bounding boxes
-    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-        box = [round(i, 2) for i in box.tolist()]
-        st.write(
-            f"Detected {model.config.id2label[label.item()]} with confidence "
-            f"{round(score.item(), 3)} at location {box}"
-        )
+        # Display the detected objects and their bounding boxes
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            box = [round(i, 2) for i in box.tolist()]
+            st.write(
+                f"Detected {model.config.id2label[label.item()]} with confidence "
+                f"{round(score.item(), 3)} at location {box}"
+            )
 
-    # Display the input image
-    st.image(image, caption="Input Image", use_column_width=True)
+        # Display the input image
+        st.image(image, caption="Input Image", use_column_width=True)
+
+else:
+    # Display an input file uploader for the image
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        # Load the uploaded image
+        image = Image.open(uploaded_file)
+
+        # Process the image using the YOLO model
+        inputs = image_processor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
+
+        # Predict bounding boxes and classes
+        logits = outputs.logits
+        bboxes = outputs.pred_boxes
+
+        # Post-process the object detection results
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
+
+        # Display the detected objects and their bounding boxes
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            box = [round(i, 2) for i in box.tolist()]
+            st.write(
+                f"Detected {model.config.id2label[label.item()]} with confidence "
+                f"{round(score.item(), 3)} at location {box}"
+            )
+
+        # Display the input image
+        st.image(image, caption="Input Image", use_column_width=True)
